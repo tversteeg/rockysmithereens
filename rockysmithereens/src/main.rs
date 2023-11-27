7edeca1dfa23a9e897bff36c6f236c1a9308ccf9
Thomas Versteeg <t@versteeg.email>
2023-11-27 20:35:52 +0100

    refactor!: start rewrite removing bevy

--- rockysmithereens/src/main.rs
+++ rockysmithereens/src/main.rs
@@ -1,130 +1,81 @@
-mod asset;
-mod filesystem;
-mod note;
-mod note_view;
-mod player;
-mod preview;
 mod ui;
-mod wem;
 
-use std::{path::PathBuf, sync::Mutex};
-
-#[cfg(feature = "profile")]
-use bevy_puffin::PuffinTracePlugin;
-
-use asset::{RocksmithAsset, RocksmithAssetLoader};
-use bevy::{
-    asset::AssetPlugin,
-    log::LogPlugin,
-    prelude::{AddAsset, App, AssetServer, Assets, Handle, Res, ResMut},
-    DefaultPlugins,
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Mutex,
 };
-use bevy_egui::EguiPlugin;
-use clap::Parser;
-use filesystem::FilesystemPlugin;
-use note::NotePlugin;
-use note_view::NoteViewPlugin;
-use player::PlayerPlugin;
 
-use preview::PreviewPlugin;
-use rockysmithereens_parser::SongFile;
-use ui::UiPlugin;
-use wem::WemPlugin;
+use miette::Result;
+use pixel_game_lib::{
+    gui::{
+        button::{Button, ButtonRef},
+        label::{Label, LabelRef},
+        Gui, GuiBuilder, Widget,
+    },
+    vek::{Extent2, Vec2},
+    window::{KeyCode, WindowConfig},
+};
+use rfd::AsyncFileDialog;
+use taffy::{prelude::Size, style::Style};
+use ui::home::Homescreen;
 
-/// Command line arguments.
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None, propagate_version = true)]
-struct Cli {
-    /// Path to a Rocksmith '*.psarc' file.
-    #[clap(value_parser)]
-    path: Option<PathBuf>,
-}
-
-/// Which phase of the game we are in.
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum Phase {
-    /// No song has been chosen yet.
-    SongSelectionMenu,
-    /// A song has been selected but no arrangement yet.
-    ArrangementSelectionMenu,
-    /// We are parsing and loading the required files.
-    Loading,
-    /// A song will be playing now.
-    Playing,
-}
-
-/// Game state.
-#[derive(Default)]
+/// Game state passed around the update and render functions.
 pub struct State {
-    /// Song asset.
-    handle: Handle<RocksmithAsset>,
-    /// Which song got selected.
-    current_song: Option<usize>,
-    /// The current difficulty.
-    difficulty: usize,
+    /// Gui for the homescreen.
+    homescreen: Homescreen,
 }
 
-// TODO: figure out how to make this a resource, the current problem is that AssetIo doesn't accept
-// any state
-lazy_static::lazy_static! {
-    /// The song state.
-    pub static ref LOADED_SONG: Mutex<Option<SongFile>> = Mutex::new(None);
-}
+/// Open an empty window.
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Window configuration with default pixel size and scaling
+    let window_config = WindowConfig {
+        buffer_size: Extent2::new(600, 480),
+        ..Default::default()
+    };
 
-fn main() {
-    let mut app = App::new();
+    // Create the shareable game state
+    let state = State {
+        homescreen: Homescreen::new(window_config.buffer_size.as_()),
+    };
 
-    // Profiling
-    #[cfg(feature = "profile")]
-    {
-        app = app.add_plugin(PuffinTracePlugin::new());
-    }
+    // Open a new thread waiting for the file dialog to be activated
+    let (open_file_tx, open_file_rx) = tokio::sync::watch::channel(());
+    tokio::spawn(async move {
+        // Repeat this task for ever
+        let mut open_file_rx = open_file_rx.clone();
+        loop {
+            // Wait for the value to "change"
+            open_file_rx.changed().await.expect("Sender dropped");
 
-    app.add_plugins_with(DefaultPlugins, |group| {
-        // Disable the logging because we'll be using puffin
-        group
-            .disable::<LogPlugin>()
-            // Insert the custom filesystem asset plugin at the right position
-            .add_before::<AssetPlugin, _>(FilesystemPlugin)
-    })
-    .add_plugin(EguiPlugin)
-    .add_plugin(WemPlugin)
-    .add_plugin(PlayerPlugin)
-    .add_plugin(UiPlugin)
-    .add_plugin(PreviewPlugin)
-    .add_plugin(NoteViewPlugin)
-    .add_plugin(NotePlugin)
-    .add_state(Phase::SongSelectionMenu)
-    .init_resource::<State>()
-    .add_asset::<RocksmithAsset>()
-    .init_asset_loader::<RocksmithAssetLoader>()
-    .add_startup_system(cli_setup)
-    .add_system(song_loader)
-    .run();
-}
+            // Open the file dialog
+            if let Some(file) = AsyncFileDialog::new()
+                .add_filter("Rocksmith", &["psarc"])
+                .pick_file()
+                .await
+            {}
+        }
+    });
 
-/// Handle CLI arguments.
-fn cli_setup(
-    mut state: ResMut<State>,
-    asset_server: Res<AssetServer>,
-    mut phase: ResMut<bevy::prelude::State<Phase>>,
-) {
-    // Parse command line arguments
-    let cli = Cli::parse();
+    // Open the window and start the game-loop
+    pixel_game_lib::window(
+        state,
+        window_config.clone(),
+        // Update loop exposing input events we can handle, this is where you would handle the game logic
+        move |state, input, mouse_pos, _dt| {
+            if state.homescreen.update(input, mouse_pos) {
+                // Release the lock for the blocking async thread
+                open_file_tx.send(()).expect("Readers dropped");
+            }
 
-    // Load the asset if set
-    if let Some(path) = cli.path {
-        state.handle = asset_server.load::<RocksmithAsset, _>(&*path);
+            // Exit when escape is pressed
+            input.key_pressed(KeyCode::Escape)
+        },
+        // Render loop exposing the pixel buffer we can mutate
+        move |state, canvas, _dt| {
+            state.homescreen.render(canvas);
+        },
+    )?;
 
-        phase.set(Phase::ArrangementSelectionMenu).unwrap();
-    }
-}
-
-/// Event listener for switching to the song virtual filesystem.
-pub fn song_loader(state: ResMut<State>, mut rocksmith_assets: ResMut<Assets<RocksmithAsset>>) {
-    // Move the asset to this filesystem
-    let asset = rocksmith_assets.remove(&state.handle);
-    if let Some(file) = asset {
-        *LOADED_SONG.lock().unwrap() = Some(file.0);
-    }
+    Ok(())
 }
---
+++ rockysmithereens/src/ui/home.rs
@@ -0,0 +1,80 @@
+use pixel_game_lib::{
+    canvas::Canvas,
+    gui::{
+        button::{Button, ButtonRef},
+        label::{Label, LabelRef},
+        Gui, GuiBuilder, Widget,
+    },
+    vek::{Extent2, Vec2},
+    window::Input,
+};
+use taffy::{prelude::Size, style::Style};
+
+/// Gui state for the homescreen.
+pub struct Homescreen {
+    /// Window size.
+    window_size: Extent2<f32>,
+    /// Button reference for opening the file.
+    open_file_button_node: ButtonRef,
+    /// Actual Gui.
+    gui: Gui,
+}
+
+impl Homescreen {
+    /// Create the home screen.
+    pub fn new(window_size: Extent2<f32>) -> Self {
+        // Create a new Gui
+        let mut gui = GuiBuilder::new(Style {
+            // Use the amount of pixels as the calculation size
+            size: Size::from_points(window_size.w, window_size.h),
+            ..Default::default()
+        });
+
+        // Create a button attached to the root
+        let open_file_button_node = gui
+            .add_widget::<ButtonRef>(
+                Button {
+                    label: Some("Load .psarc file".to_string()),
+                    ..Default::default()
+                },
+                Style {
+                    size: Size::from_points(300.0, 20.0),
+                    ..Default::default()
+                },
+                gui.root(),
+            )
+            .unwrap();
+
+        let gui = gui.build();
+
+        Self {
+            gui,
+            window_size,
+            open_file_button_node,
+        }
+    }
+
+    /// Update the Gui.
+    ///
+    /// Returns whether to trigger the open window.
+    pub fn update(&mut self, input: &Input, mouse_pos: Option<Vec2<usize>>) -> bool {
+        // Update the GUI to fill the screen
+        self.gui.update_layout(Vec2::zero(), self.window_size.as_());
+
+        // Update the button manually
+        let button: &mut Button = self.gui.widget_mut(self.open_file_button_node).unwrap();
+
+        // Handle the button press
+        button.update(input, mouse_pos)
+    }
+
+    /// Render the Gui.
+    pub fn render(&mut self, canvas: &mut Canvas) {
+        // Reset the canvas
+        canvas.fill(0xFFFFFFFF);
+
+        // Render the button manually
+        let button: &Button = self.gui.widget(self.open_file_button_node).unwrap();
+        button.render(canvas);
+    }
+}
